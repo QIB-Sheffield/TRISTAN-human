21075e46cc02a02b47aeb7f04a3c22332024e2f7
Eve Shalom <79933818+EShalom@users.noreply.github.com>
2025-01-17 05:01:01 +0000

    Add steps which are updated/new
    * may need to adjust steps into seperate scripts in need manual
      segmentations
    
    * some step depend on both scans e.g. making xlsx of each visit
    
    * currently memory intensive motion_correction likely will not run for
      TRISTAN data on laptop recommend HPC

--- src/main.py
+++ src/main.py
@@ -2,29 +2,44 @@ from scripts import steps
 
 def main(subject_details, study_name):
 
+    scan = subject_details[2]
+
     # setup logger
     logger = steps.setup_logger(subject_details)
 
     # setup details dictionary
     info = steps.setup_detail_dict(subject_details, study_name, logger)
 
-    # # extract arrays from dicom
+    # extract arrays from dicom
     steps.extract_arrays(info)
 
-    # # segment ROI pre coregistration
-    # steps.segment_ROI(info, 'pre_coreg')
-
-    # # setup mdreg
-    # steps.correct_motion(info)
-
-    # segment ROI post coregistration
-    #steps.segment_ROI(info, 'post_coreg')
+    # segment ROI pre coregistration
+    steps.segment_ROI(info, 'pre_coreg')
 
     # show ROIS
-    #steps.show_ROIS(info, 'postcoreg')
+    steps.show_ROIS(info, 'pre_coreg')
+
+    # setup mdreg
+    steps.correct_motion(info, [20,90])
+
+    # segment ROI post coregistration
+    steps.segment_ROI(info, 'post_coreg')
+
+    # show ROIS
+    steps.show_ROIS(info, 'post_coreg')
 
     # Map T1
-    steps.t1mapping_molli(info)
+    steps.t1mapping_molli(info, 'pre_coreg')
+
+    if scan == 1:
+        steps.t1mapping_molli(info, 'post_coreg')
+
+    if scan == 2:
+        # export CSV
+        # involves both scans 1 and 2
+        steps.format_to_csv(info)
+        steps.run_dcmri(info)
+
 
     return
 
@@ -33,7 +48,7 @@ if __name__ == '__main__':
 
     subjects = [5]
     visits = [1]
-    scans = [1, 2]
+    scans = [1,2]
 
     study_name = 'tristan_twocomp'
 
