e575338962f39207fa23d9d25fc0d22e9c15432f
Eve Shalom <79933818+EShalom@users.noreply.github.com>
2025-01-17 04:58:51 +0000

    Upgrade error reporting and add dcmri wrapper
    *return full lerror and line traceback into log file

--- src/scripts/steps.py
+++ src/scripts/steps.py
@@ -1,6 +1,6 @@
 import time
 import traceback
-from pipelines import log, export, segment, csv, motioncorrect, plotting, mapping
+from pipelines import log, export, segment, csv, motioncorrect, plotting, mapping, analysis
 from utilities import helper
 
 ## SETUP LOGGER
@@ -12,6 +12,8 @@ def setup_logger(subject_details):
     except Exception as e:
         print("An error occurred:")
         traceback.print_exc()
+        error_message = traceback.format_exc()
+        print(error_message)
         logger = None
 
     return logger
@@ -26,8 +28,10 @@ def setup_detail_dict(subject_details, study_name, logger=None):
     except Exception as e:
         print("An error occurred:")
         traceback.print_exc()
+        error_message = traceback.format_exc()
         if logger:
             logger.error('Error setting up details dictionary')
+            logger.error(error_message)
         info = None
 
     return info
@@ -45,8 +49,10 @@ def extract_arrays(info):
     except Exception as e:
         print("An error occurred:")
         traceback.print_exc()
+        error_message = traceback.format_exc()
         if info['logger']:
             info['logger'].error('Error extracting arrays from dicom')
+            info['logger'].error(error_message)
         pass
 
     return
@@ -63,41 +69,47 @@ def segment_ROI(info, seg_type):
     except Exception as e:
         print("An error occurred:")
         traceback.print_exc()
+        error_message = traceback.format_exc()
         if info['logger']:
             info['logger'].error('Error segmenting ROI')
+            info['logger'].error(error_message)
         pass
 
     return
 
 ## MOTION CORRECTION
 
-def correct_motion(info):
+def correct_motion(info, fit_interval=None):
     start_time = time.time()
     try:
-        motioncorrect.setup_mdreg(info)
+        motioncorrect.setup_mdreg(info, fit_interval)
         total_time = time.time() - start_time
         if info['logger']:
             info['logger'].info('Completed Motion Correction via mdreg in {} seconds'.format(total_time))
     except Exception as e:
         print("An error occured:")
         traceback.print_exc()
+        error_message = traceback.format_exc()
         if info['logger']:
             info['logger'].error('Error completing motion correction')
+            info['logger'].error(error_message)
         pass
     return
     
-def t1mapping_molli(info):
+def t1mapping_molli(info, scan_type):
     start_time = time.time()
     try:
-        mapping.map_molli(info, 'pre_contrast')
+        mapping.map_molli(info, scan_type)
         total_time = time.time() - start_time
         if info['logger']:
             info['logger'].info('Completed Motion Correction via mdreg in {} seconds'.format(total_time))
     except Exception as e:
         print("An error occured:")
         traceback.print_exc()
+        error_message = traceback.format_exc()
         if info['logger']:
             info['logger'].error('Error completing motion correction')
+            info['logger'].error(error_message)
     
 ## REFORMAT TO CSV
 
@@ -111,8 +123,10 @@ def format_to_csv(info):
     except Exception as e:
         print("An error occured:")
         traceback.print_exc()
+        error_message = traceback.format_exc()
         if info['logger']:
             info['logger'].error('Error completing data reformatting to CSV')
+            info['logger'].error(error_message)
         pass
     return
 
@@ -128,9 +142,34 @@ def show_ROIS(info, checkpoint):
     except Exception as e:
         print("An error occured:")
         traceback.print_exc()
+        error_message = traceback.format_exc()
         if info['logger']:
             info['logger'].error('Error showing ROIS')
+            info['logger'].error(error_message)
         pass
+    return
+
+## CONVERT XLSX TO DCMRI FORMAT
+
+def run_dcmri(info):
+    start_time = time.time()
+    try:
+        analysis.dcmri_analysis(info)
+        total_time = time.time() - start_time
+        if info['logger']:
+            info['logger'].info('Completed dcmri analysis in {} seconds'.format(total_time))
+    except Exception as e:
+        print("An error occured:")
+        traceback.print_exc()
+        error_message = traceback.format_exc()
+        if info['logger']:
+            info['logger'].error('Error in dcmri')
+            info['logger'].error(error_message)
+        pass
+
+    if info['logger']:
+        info['logger'].info('IMPLEMENT DCMRI ANALYSIS FUNCTIONALITY HERE')
+
     return
 
 
