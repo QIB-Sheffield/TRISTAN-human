28ee2ceb0670346bb147e19403a87b5c941626f5
Eve Shalom <79933818+EShalom@users.noreply.github.com>
2025-01-17 04:51:16 +0000

    Fix bug and paths in T1 fitting
    * mdreg seems only running in debug mode vscode
      - could be my machine maybe not package wide issue
    
    * export arrays into molli directory
    
    * need extraction to ROI matching DCE, see in code comments

--- src/pipelines/mapping.py
+++ src/pipelines/mapping.py
@@ -3,8 +3,11 @@ import os
 from utilities import helper
 import numpy as np
 
-def map_molli(info, checkpoint):
-    
+def map_molli(info, scan_type):
+
+    logger = info['logger']    
+    subject, visit, scan = info['subject'], info['visit'], info['scan']
+
     array_path = os.path.join(info['output_path'], 'arrays')
     subject_path = info['subject_path']
     output_path = info['output_path']
@@ -14,11 +17,25 @@ def map_molli(info, checkpoint):
 
     file_list = helper.list_files(scan_path)
 
+
+    if scan_type == 'pre_coreg':
+        dicom_name = ''
+        scan_tag = 'prec'
+    elif scan_type == 'post_coreg':
+        dicom_name = 'POST'
+        scan_tag = 'postc'
+
+    molli_output_path = os.path.join(output_path, 'molli')
+    helper.check_dirs_exist(molli_output_path)
+
+    molli_file = os.path.join(molli_output_path, f'S{subject}_V{visit}_S{scan}_{scan_type}_molli.npz')
+
+    # Load the correct MOLLI array and corresponding inversion times
     for filename in file_list:
-        if 'LL' in filename and checkpoint in filename:
+        if 'LL' in filename and dicom_name in filename:
             molli_array = np.load(filename)
         
-        if 'LL' in filename and 'inversion_times' in filename:
+        if 'inversion_times' in filename and scan_tag in filename:
             inv_times = np.load(filename)['inversion_times']
             spacing = np.load(filename)['spacing']
     
@@ -26,13 +43,14 @@ def map_molli(info, checkpoint):
     # The function to fit the data
     'func': mdreg.abs_exp_recovery_2p,
     # The keyword arguments required by the function
-    'TI': inv_times/1000,
+    'TI': inv_times/1000, # Convert to seconds from ms
     }
     
+    # Set the coregistration parameters to use mdreg
     coreg_params = {
         'package': 'elastix',
         'FinalGridSpacingInPhysicalUnits': 50.0,
-        'spacing': spacing
+        'spacing': spacing,
     }
 
     plot_settings = {
@@ -42,14 +60,28 @@ def map_molli(info, checkpoint):
         'show' : False,
     }
 
-    coreg, defo, fit, pars = mdreg.fit(molli_array,
+    coreg, defo, fit, pars = mdreg.fit(molli_array[:,:,0,:],
                                     fit_image=molli, 
                                     fit_coreg=coreg_params,
                                     plot_params=plot_settings,
                                     maxit=5, 
                                     verbose=2)
 
-    np.savez_compressed(f'{output_path}\\coreg\\{checkpoint}_molli.npz', coreg=coreg, pars=pars)
-    mdreg.plot_series(molli_array, fit, coreg, defo, plot_settings)
+    # Save the coregistration and fit parameters
+    # Fit parameters are the T1 and M0 values
+
+    np.savez(molli_file, coreg=coreg, S0=pars[..., 0], T1=pars[..., 1], fit=fit)
+    logger.info(f'{scan_type} MOLLI mapping complete')
+
+    logger.info('TO DO: implement export of ROI T1 values to .npz files as needed for XLSX export')
+    logger.info('requires affine and vreg/dbdicom to select reiggion from dce mask')
+
+    # TO DO implement export of ROI T1 values to .npz files as needed for XLSX export
+    # requires affine and vreg/dbdicom to select reiggion from dce mask
+    # Or similiar if changing the export method in csv.py
+
+    # example:
+    # some segmentation extraction here
+    # np.savez_compressed(os.path.join(output_path, 'molli', f'S{subject}_V{visit}_S{scan}_{scan_type}_molli_ROIvalues.npz'), time=timepoints, liver=liver_value, aorta=aorta_value)
 
     return
